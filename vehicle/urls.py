dc46cc8faea3d8f2c4cf99a5c7baa8190f98ba11
vasilij korobov <kavyabass51@gmail.com>
2025-05-04 12:03:33 +0700

    Задача Создать модель «Список машин» и реализовать механизм CRUD на основе ViewSet.

---
+++ vehicle/urls.py
@@ -0,0 +1,14 @@
+from vehicle.apps import VehicleConfig
+from rest_framework.routers import DefaultRouter
+
+from vehicle.views import CarViewSet
+
+app_name = VehicleConfig.name
+
+
+router = DefaultRouter()
+router.register(r'cars', CarViewSet, basename='cars')
+
+urlpatterns = [
+
+] + router.urls
