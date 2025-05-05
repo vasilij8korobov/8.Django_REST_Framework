57e9f3f72bb27abd068867f3de3b88ad3af409be
vasilij korobov <kavyabass51@gmail.com>
2025-05-06 00:01:34 +0700

    Для мотоцикла и для машины выводить последний записанный пробег.

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -4,6 +4,7 @@ from vehicle.models import Car, Moto, Milage
 
 
 class CarSerializer(serializers.ModelSerializer):
+    last_milage = serializers.IntegerField(source='milage_set.all.first.milage')
 
     class Meta:
         model = Car
@@ -11,14 +12,19 @@ class CarSerializer(serializers.ModelSerializer):
 
 
 class MotoSerializer(serializers.ModelSerializer):
+    last_milage = serializers.SerializerMethodField()
 
     class Meta:
         model = Moto
         fields = '__all__'
 
+    def get_last_milage(self, instance):
+        if instance.milage_set.all().first():
+            return instance.milage_set.all().first().milage
+        return 0
+
 
 class MilageSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Milage
         fields = '__all__'
