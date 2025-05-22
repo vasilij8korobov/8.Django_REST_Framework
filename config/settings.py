490d41fb6cb789cd00cad9a8a65a4481e9116b33
vasilij korobov <kavyabass51@gmail.com>
2025-05-22 21:34:32 +0700

    Задача Подключить Celery к проекту для дальнейшей работы.

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,20 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CUR_API_URL = 'https://app.currencyapi.com/'
 CUR_API_KEY = 'cur_live_SDMkTTtFBEMUiMtWNLBIkFDhzeHKUGrK1cpgzyMh'
+
+# Настройки для Celery
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379' # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Australia/Tasmania"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
